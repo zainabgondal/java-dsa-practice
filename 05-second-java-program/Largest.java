999ec410ebc9d601a65ea8e248f456ae25d2b2a3
Zainab Gondal <gondalzainab34@gmail.com>
2026-05-30 15:54:39 +0500

    Add files via upload

---
+++ 05-second-java-program/Largest.java
@@ -0,0 +1,31 @@
+package com.rahul;
+
+import java.util.Scanner;
+
+public class Largest {
+    static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter Num1: ");
+//        int a = sc.nextInt();
+//        System.out.print("Enter Num2: ");
+//        int b = sc.nextInt();
+//        System.out.print("Enter Num3: ");
+//        int c = sc.nextInt();
+
+//        int max = a;
+//        if (b > max){
+//            max = b;
+//        }
+//        if (c > max){
+//            max = c;
+//        }
+
+//        int max = Math.max(c, Math.max(a,b));
+
+//        System.out.println("The Largest Number is: " + max);
+
+        int d;
+        int e;
+        System.out.println(Math.max(34,67));
+    }
+}
